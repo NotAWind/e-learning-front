e0688befb5a166aaaa31602349a03cb335342640
lihong xu <3466141946@qq.com>
2024-06-27 16:43:13 +0100

    feature: finish addUser static page

--- app/admin/addUser/page.tsx
+++ app/admin/addUser/page.tsx
@@ -3,8 +3,15 @@ import * as React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
+import MultipleSelector, { Option } from "@/components/ui/multi-selector";
+import { LoadingButton } from "@/components/ui/loading-button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   Form,
   FormControl,
@@ -16,49 +23,143 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+const schoolOptionSchema = z.object({
+  label: z.string(),
+  value: z.string(),
+  disable: z.boolean().optional(),
+});
+
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
+  email: z.string().email({ message: "Invalid email address" }),
+  role: z.string({
+    required_error: "Please select an role to display.",
+  }),
+  school: z.array(schoolOptionSchema).min(1),
 });
 
+const OPTIONS: Option[] = [
+  { label: "school A", value: "school A" },
+  { label: "school B", value: "school B" },
+  { label: "school C", value: "school C" },
+  { label: "school D", value: "school D" },
+];
+
 export default function AddUser() {
-  // 1. Define your form.
+  const [loading, setLoading] = React.useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      email: "",
+      role: "",
+      school: [],
     },
+    mode: "onChange",
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      console.log(JSON.stringify(data, null, 2));
+    }, 500);
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="px-10 pt-10">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="please enter user's name" {...field} />
+                </FormControl>
+                <FormDescription>This is usesr's name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="please enter user's email" {...field} />
+                </FormControl>
+                <FormDescription>This is user's email.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="role"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Role</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select the user's role" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="student">Student</SelectItem>
+                    <SelectItem value="teacher">Teacher</SelectItem>
+                    <SelectItem value="admin">Admin</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormDescription>
+                  Different roles have different permissions.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="school"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Schools</FormLabel>
+                <FormControl>
+                  <MultipleSelector
+                    {...field}
+                    defaultOptions={OPTIONS}
+                    placeholder="Select the schools that the user has permission to view"
+                    emptyIndicator={
+                      <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                        no results found.
+                      </p>
+                    }
+                  />
+                </FormControl>
+                <FormDescription>
+                  Selecting a school means the user has permission to view the
+                  various courses or information published by that school.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <LoadingButton loading={loading} type="submit">
+            Submit
+          </LoadingButton>
+        </form>
+      </Form>
+    </div>
   );
 }

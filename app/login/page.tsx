c37804099d814f08b210dae5986c0677b6dba1bf
lihong xu <3466141946@qq.com>
2024-07-13 22:20:36 +0100

    feature: finish login page mock data integration

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import {
   Card,
@@ -13,8 +16,44 @@ import { Input } from "@/components/ui/input";
 import style from "./page.module.css";
 import { Label } from "@/components/ui/label";
 import { School, User, LogIn } from "lucide-react";
+import { RequestPrefix } from "@/app/utils/request";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("admin");
+  const { toast } = useToast();
+
+  const handleLogin = async (e: any) => {
+    const response = await fetch(
+      `${RequestPrefix}/users?email=${email}&password=${password}`
+    );
+
+    console.log(response);
+    const users = await response.json();
+
+    if (users.length > 0) {
+      const user = users[0];
+      if (role === "student" && user.role === role) {
+        alert(`Welcome student`);
+      } else if (role === "admin" && user.role === "teacher") {
+        alert(`Welcome teacher`);
+      } else if (role === "admin" && user.role === "admin") {
+        alert(`Welcome admin`);
+      } else {
+        alert("Incorrect role");
+      }
+    } else {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "Invalid email or password",
+        className: "z-10",
+      });
+    }
+  };
+
   return (
     <main className={style.main}>
       <div className={style.logo}>
@@ -39,18 +78,19 @@ export default function Login() {
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="role">Role</Label>
                 <RadioGroup
-                  defaultValue="card"
+                  defaultValue="admin"
                   className="grid grid-cols-3 gap-4"
+                  onValueChange={setRole}
                 >
                   <div>
                     <RadioGroupItem
-                      value="card"
-                      id="card"
+                      value="admin"
+                      id="admin"
                       className="peer sr-only"
-                      aria-label="Card"
+                      aria-label="Admin"
                     />
                     <Label
-                      htmlFor="card"
+                      htmlFor="admin"
                       className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
                     >
                       <School color="green" size={48} />
@@ -59,13 +99,13 @@ export default function Login() {
                   </div>
                   <div>
                     <RadioGroupItem
-                      value="stuent"
-                      id="stuent"
+                      value="student"
+                      id="student"
                       className="peer sr-only"
-                      aria-label="Stuent"
+                      aria-label="Student"
                     />
                     <Label
-                      htmlFor="stuent"
+                      htmlFor="student"
                       className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary "
                     >
                       <User color="green" size={48} />
@@ -76,17 +116,29 @@ export default function Login() {
               </div>
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" placeholder="Email" type="email" />
+                <Input
+                  id="email"
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="framework">Password</Label>
-                <Input id="password" placeholder="Password" type="password" />
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">
+          <Button className="w-full" onClick={handleLogin}>
             <LogIn className="mr-2 h-4 w-4" /> Login
           </Button>
         </CardFooter>
---
+++ app/utils/request.ts
@@ -0,0 +1,1 @@
+export const RequestPrefix = "http://localhost:3001/api";
